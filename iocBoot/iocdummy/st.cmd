997fbf317afcda4f2956cf7c399550823006078b
Hinko Kocevar <hinko.kocevar@ess.eu>
2023-03-24 09:23:06 +0100

    initial import

---
+++ iocBoot/iocdummy/st.cmd
@@ -0,0 +1,18 @@
+#!../../bin/linux-x86_64/dummy
+
+#- You may have to change dummy to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/dummy.dbd"
+dummy_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadTemplate "db/wf.substitutions"
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
